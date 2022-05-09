df76c2b5e7a962cfce9174c061531f0f665e2eff
EngOmondi <fidelotieno167@gmail.com>
2022-05-09 17:52:25 +0300

    modified the project

--- account/models.py
+++ account/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.hashers import make_password
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
 
 
 class CustomUserManager(UserManager):
